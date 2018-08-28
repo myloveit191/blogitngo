1f41fa61623237f92695eb74d19d3a8cdc6b551a
ddnghia <nhabannuocmam@gmail.com>
2018-08-29 00:00:50 +0700

    css admin

--- wp-content/plugins/blogit/templates/admin.php
+++ wp-content/plugins/blogit/templates/admin.php
@@ -1,15 +1,30 @@
 <div class="wrap">
     <h1>BlogIt Plugin</h1>
     <?php 
-    // include_once( dirname(__FILE__,5) . '/wp-admin/options.php' );
     settings_errors();?>
-    <form action="options.php" method="post">
-        <table class="form-table">
-        <?php 
-            settings_fields("blogit_options_group");
-            do_settings_sections('blogit-plugin');
-        ?>
-        </table>
-        <?php  submit_button(); ?>
-    </form>
+    <ul class="nav nav-tabs">
+        <li class="active"><a href="#tab-1">Manage Settings</a></li>
+        <li><a href="#tab-2">Updates</a></li>
+        <li><a href="#tab-3">About</a></li>
+    </ul> 
+    <div class="tab-content">
+        <div id="tab-1" class="tab-pane active">
+            <form action="options.php" method="post">
+                <table class="form-table">
+                <?php 
+                    settings_fields("blogit_options_group");
+                    do_settings_sections('blogit-plugin');
+                ?>
+                </table>
+                <?php  submit_button(); ?>
+            </form>
+        </div>
+        <div id="tab-2" class="tab-pane">
+            <h3>Updates</h3>
+        </div>
+        <div id="tab-3" class="tab-pane">
+            <h3>Abouts</h3>
+        </div>
+    </div>
+    
 </div>
